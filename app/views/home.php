09a871da0c0643a0aeb6b8e48fa461e0a2043f17
karita2005 <129924034+karita2005@users.noreply.github.com>
2026-06-15 18:49:09 +0100

    Add home view

---
+++ app/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Welcome to PHP MVC Project!</h1>
+    <p>This is a basic MVC structure.</p>
+</body>
+</html>
